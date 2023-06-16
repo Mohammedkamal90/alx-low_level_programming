b632b6ec8f6f050c4f54d2d0a18d754b1921108e
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-06-15 17:36:06 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,39 +9,29 @@
  *return: pointer to space in memory
  */
 
-char *string_concat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *str;
-unsigned int i, j, s1_length, s2_length;
-/* check if string passed is null */
-if (s1 == NULL)
-	s1 = "";
-if (s2 == NULL)
-	s2 == "";
-/*compute the length of strings  */
-
-for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
-	;
-
-for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
-	;
-
-/*memory reserving for 1&2*/
-
-str = malloc(s1_length + n + 1);
-if (str == NULL)
+char *s;
+unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+while (s1 && s1[len1])
+len1++;
+while (s2 && s2[len2])
+len2++;
+if (n < len2)
+s = malloc(sizeof(char) * (len1 + n + 1));
+else
+s = malloc(sizeof(char) * (len1 + len2 + 1));
+if (!s)
+return (NULL);
+while (i < len1)
 {
-	return (NULL);
+s[i] = s1[i];
+i++;
 }
-/*copy first string  */
-for (i = 0; s1[i] != '\0'; i++)
-	str[i] = s1[i];
-/*copy second string  */
-for (j = 0; j < n; j++)
-{
-	str[i] = s2[j];
-	i++;
-}
-str[i] = '\0';
-return (str);
+while (n < len2 && i < (len1 + n))
+s[i++] = s2[j++];
+while (n >= len2 && i < (len1 + len2))
+s[i++] = s2[j++];
+s[i] = '\0';
+return (s);
 }
