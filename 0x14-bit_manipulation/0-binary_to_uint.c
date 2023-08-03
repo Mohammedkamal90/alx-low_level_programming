4ad2db54daf9af6f27ad0cb08cb87311e478ed98
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2023-08-02 22:51:29 -0700

    binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_uint - convert bin number to unsign int
+ * @b: string containing bin number
+ * Return: converted num
+ */
+unsigned int binary_uint(const char *b)
+{
+	int i;
+	unsigned int dec_val = 0;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
+
+	return (dec_val);
+}
